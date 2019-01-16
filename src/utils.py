611da5e92724685d09c73f25f3a62423e24dfaba
ahmed_besbes <ahmed.besbes@capgemini.com>
2019-01-16 13:41:48 +0100

    adding text preprocessing functions

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,20 @@
+import re
+
 def process_text(text):
     processed_text = ""
-    for tx in text
+    for tx in text:
         processed_text += tx + " "
     return processed_text
+
+def remove_hashtags():
+    clean_text = re.sub(r"#[A-Za-z0-9_]+", "", text)
+    return clean_text
+
+def remove_user_mentions(text):
+    clean_text = re.sub(r"@[A-Za-z0-9_]+", "", text)
+    return clean_text
+    
+def remove_urls(text):
+    clean_text = re.sub(r'^https?:\/\/.*[\r\n]*', '', text, flags=re.MULTILINE)
+    return clean_text
+    
