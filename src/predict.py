fixing predict to output probabilities using softmax

src/predict.py
@@ -1,24 +1,29 @@
 import argparse
 import torch
 import utils
+import torch.nn.functional as F
 
 
 def predict(args):
     model = torch.load(args.model)
     processed_input = utils.preprocess_input(args)
-    prediction = model.forward(processed_input)
-    return prediction
+    processed_input = torch.tensor(processed_input)
+    processed_input = processed_input.unsqueeze(0)
+    prediction = model(processed_input)
+    probabilities = F.softmax(prediction, dim=1)
+    return probabilities
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         'Testing a pretrained Character Based CNN for text classification')
+    parser.add_argument('--model', type=str, help='path for pre-trained model')
+    
     parser.add_argument('--text', type=str,
                         default='I love pizza!', help='text string')
-    parser.add_argument('doc_type', type=str,
-                        choices=['tweets', 'reviews'], default='tweets')
+    parser.add_argument('--doc_type', type=str,
+                        choices=['tweet', 'review'], default='tweet')
 
-    parser.add_argument('--model', type=str, help='path for pre-trained model')
     parser.add_argument('--input_file', type=str,
                         help='path of a csv input file containing data to score')
     parser.add_argument('--output_file', type=str,

src/utils.py
@@ -66,6 +66,17 @@ def get_evaluation(y_true, y_prob, list_metrics):
 
 def preprocess_input(args):
     raw_text = args.text
+    if args.doc_type == 'tweet':
+        steps = ['remove_hashtags',
+                 'remove_urls',
+                 'remove_user_mentions',
+                 'lower']
+    elif args.doc_type == 'review':
+        steps = ['remove_urls',
+                 'lower']
+    for step in steps:
+        raw_text = preprocessing_setps[step](raw_text)
+
     number_of_characters = args.number_of_characters
     identity_mat = np.identity(number_of_characters)
     vocabulary = list(args.alphabet)
@@ -82,5 +93,3 @@ def preprocess_input(args):
         processed_output = np.zeros(
             (max_length, number_of_characters), dtype=np.float32)
     return processed_output
-
-    