b4a9b36936efc9deac7daf48a926e09f9a94e989
Ahmed Besbes <ahmed.besbes@hotmail.com>
2019-10-15 11:58:58 +0200

    remove import

--- train.py
+++ train.py
@@ -17,7 +17,6 @@ from tensorboardX import SummaryWriter
 from sklearn.metrics import classification_report, f1_score
 from sklearn.model_selection import train_test_split
 
-from src.cnn_model import CharacterLevelCNN
 from src.data_loader import MyDataset, load_data
 from src import utils
 from src.model import CharacterLevelCNN
