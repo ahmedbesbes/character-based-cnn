c9fb3fc38aa6747bbf142a9913acb1f74c4365c5
Ahmed Besbes <ahmed.besbes@hotmail.com>
2019-10-19 21:11:35 +0200

    print texts

--- train.py
+++ train.py
@@ -205,6 +205,9 @@ def run(args, both_cases=False):
                          "num_workers": args.workers}
 
     texts, labels, number_of_classes, sample_weights = load_data(args)
+
+    print(f'5 first comments : {texts[:5]}')
+
     train_texts, val_texts, train_labels, val_labels, train_sample_weights, _ = train_test_split(texts,
                                                                                                  labels,
                                                                                                  sample_weights,
