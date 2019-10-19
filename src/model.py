1a2b277ae65f69ac5176b7cb6696f00dd57c4d32
Ahmed Besbes <ahmed.besbes@hotmail.com>
2019-10-19 21:00:33 +0200

    remove dropout attribute

--- src/model.py
+++ src/model.py
@@ -98,8 +98,6 @@ class CharacterLevelCNN(nn.Module):
         x = self.conv6(x)
         x = x.view(x.size(0), -1)
         x = self.fc1(x)
-        x = self.dropout(x)
         x = self.fc2(x)
-        x = self.dropout(x)
         x = self.fc3(x)
         return x
