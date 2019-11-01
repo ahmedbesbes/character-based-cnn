00c488d9f7607783d80eb35f7c7d53fe5cc3519c
Ahmed Besbes <ahmed.besbes@hotmail.com>
2019-11-01 15:14:55 +0100

    add ignore center

--- train.py
+++ train.py
@@ -367,6 +367,8 @@ if __name__ == "__main__":
     parser.add_argument('--steps', nargs='+', default=['lower'])
     parser.add_argument('--group_labels', type=int, default=1, choices=[0, 1])
     parser.add_argument('--ignore_center', type=int, default=1, choices=[0, 1])
+    parser.add_argument('--label_ignored', type=int, default=None)
+
     parser.add_argument('--ratio', type=float, default=1)
     parser.add_argument('--balance', type=int, default=0, choices=[0, 1])
     parser.add_argument('--use_sampler', type=int,
